663aa5734dca33f52b6a13842f9edb9992459e55
Jung Gyu Yoon <yjg30737@gmail.com>
2023-02-05 10:30:42 +0900

    Add basic style to distinguish between each widget

--- pyqt_chat_widget/chatWidget.py
+++ pyqt_chat_widget/chatWidget.py
@@ -1,6 +1,7 @@
 from PyQt5.QtCore import Qt
 from PyQt5.QtWidgets import QScrollArea, QVBoxLayout, QWidget, QLabel, QLineEdit, QHBoxLayout
 
+
 class ChatBrowser(QScrollArea):
     def __init__(self):
         super().__init__()
@@ -9,6 +10,8 @@ class ChatBrowser(QScrollArea):
     def __initUi(self):
         lay = QVBoxLayout()
         lay.setAlignment(Qt.AlignTop)
+        lay.setSpacing(0)
+        lay.setContentsMargins(0, 0, 0, 0)
         widget = QWidget()
         widget.setLayout(lay)
         self.setWidget(widget)
@@ -17,10 +20,13 @@ class ChatBrowser(QScrollArea):
     def showText(self, text, user_f):
         chatLbl = QLabel(text)
         if user_f:
+            chatLbl.setStyleSheet('QLabel { padding: 1em }')
             chatLbl.setAlignment(Qt.AlignRight)
         else:
+            chatLbl.setStyleSheet('QLabel { background-color: #DDD; padding: 1em }')
             chatLbl.setAlignment(Qt.AlignLeft)
         self.widget().layout().addWidget(chatLbl)
+
 
 class Prompt(QWidget):
     def __init__(self):
@@ -29,6 +35,7 @@ class Prompt(QWidget):
 
     def __initUi(self):
         self.__lineEdit = QLineEdit()
+        self.__lineEdit.setStyleSheet('QLineEdit { border: 1px solid #AAA; padding: 1em }')
         lay = QHBoxLayout()
         lay.addWidget(self.__lineEdit)
         lay.setContentsMargins(0, 0, 0, 0)
